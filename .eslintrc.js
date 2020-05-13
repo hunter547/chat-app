37609ad64e2c6e9264b3e946b5327e43c612b9c2
Hunter Evanoff <hunter547@gmail.com>
2020-05-13 09:27:12 -0400

    Added JSDoc and documented all functions for use by other developers

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    'react-native/react-native': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-native',
+  ],
+  rules: {
+  },
+  parser: 'babel-eslint',
+};
--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { decode, encode } from 'base-64';
 // Import the screens we want to navigate
 import Start from './components/Start';
 import Chat from './components/Chat';
 
 // imported to avoid a "can't find variable: atob" issue: https://stackoverflow.com/a/60386964
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode }
-////////////////////////////////////////////////////////
+if (!global.btoa) { global.btoa = encode; }
+if (!global.atob) { global.atob = decode; }
 
 // Create a navigator that will take care of routing the Screens declared previoulsy
 const navigator = createStackNavigator({
